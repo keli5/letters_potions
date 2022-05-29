3d18f5887b1b30f96cebce4c70728eb14153ddeb
keli5 <46039037+keli5@users.noreply.github.com>
2022-05-29 11:35:13 -0500

    size changing potions! I LOVE THE

--- lua/entities/letpot_sizedown/init.lua
+++ lua/entities/letpot_sizedown/init.lua
@@ -1,16 +1,34 @@
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+OGVZ = {}
+
 function ENT:Use(activator, caller, useType, value)
-    activator:SetModelScale(2, 0.5)
+    if OGVZ[activator] then return false end
+    activator:SetModelScale(0.5, 0.5)
+    --
+    local viewOffset = activator:GetViewOffset()
+    OGVZ[activator] = viewOffset.z
+    viewOffset.z = viewOffset.z * 0.5
+    activator:SetViewOffset(viewOffset)
     --
     self:Remove()
     --
     timer.Simple(self.PotionDuration, function()
         activator:SetModelScale(1, 0.5)
+        viewOffset = activator:GetViewOffset()
+        viewOffset.z = OGVZ[activator]
+        activator:SetViewOffset(viewOffset)
+        OGVZ[activator] = nil
     end)
 end
 
-hook.Add("DoPlayerDeath", "ResetRunSpeed", function(ply, attacker, dmginfo)
-    ply:SetModelScale(1, 0.0001)
+hook.Add("DoPlayerDeath", "ResetSize", function(ply, attacker, dmginfo)
+    if OGVZ[ply] then
+        ply:SetModelScale(1, 0.0001)
+        viewOffset = ply:GetViewOffset()
+        viewOffset.z = OGVZ[ply]
+        ply:SetViewOffset(viewOffset)
+        OGVZ[ply] = nil
+    end
 end)
