8a8ee0aad20388e8e4de8034e2c32135c86ae904
keli5 <46039037+keli5@users.noreply.github.com>
2022-05-29 09:38:40 -0500

    add shrinking and growing drink

---
+++ lua/entities/letpot_sizedown/init.lua
@@ -0,0 +1,16 @@
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:Use(activator, caller, useType, value)
+    activator:SetModelScale(2, 0.5)
+    --
+    self:Remove()
+    --
+    timer.Simple(self.PotionDuration, function()
+        activator:SetModelScale(1, 0.5)
+    end)
+end
+
+hook.Add("DoPlayerDeath", "ResetRunSpeed", function(ply, attacker, dmginfo)
+    ply:SetModelScale(1, 0.0001)
+end)
