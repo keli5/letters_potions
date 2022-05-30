4160bf910e3304d571a154cce9dd0e84de87b504
keli5 <46039037+keli5@users.noreply.github.com>
2022-05-30 13:51:14 -0500

    mixing barrel does nothing yet but it's here

--- lua/entities/letpot_mixing_barrel/init.lua
+++ lua/entities/letpot_mixing_barrel/init.lua
@@ -2,10 +2,6 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Use(activator, caller, useType, value)
-    local createdPot = ents.Create("letpot_pot_timebomb")
-    createdPot:SetPos(self:GetPos() + Vector(0, 0, 10))
-    createdPot:SetAngles(self:GetAngles())
-    createdPot:Spawn()
-    createdPot:SetPotionPotency(3)
+    
 
 end
--- lua/entities/letpot_mixing_barrel/shared.lua
+++ lua/entities/letpot_mixing_barrel/shared.lua
@@ -5,8 +5,8 @@ ENT.Category = "Letters' Drinks: Crafting"
 ENT.PrintName = "Mixing Barrel"
 
 function ENT:Initialize()
-    self.ResultPotencyMult = 1
-    self.ResultDurationMult = 1
+    self:SetResultPotencyMult(1)
+    self:SetResultDurationMult(1)
     
     self:SetModel("models/props_wasteland/laundry_washer003.mdl")
 
@@ -17,4 +17,9 @@ function ENT:Initialize()
 
     self:PhysWake()
     self:Activate()
+end
+
+function ENT:SetupDataTables()
+    self:NetworkVar("Int", 1, "ResultPotencyMult")
+    self:NetworkVar("Int", 2, "ResultDurationMult")
 end
